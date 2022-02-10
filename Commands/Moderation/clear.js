8318813a19eec88edf7067e27cff124b12510343
Gabriel Franzeri <gabriel.franzeri@elgin.com.br>
2022-02-10 18:02:32 -0300

    try catch adicionado no comando clear, erro a ser identificado

--- Commands/Moderation/clear.js
+++ Commands/Moderation/clear.js
@@ -34,27 +34,32 @@ module.exports = {
         const Response = new MessageEmbed()
         .setColor("LUMINOUS_VIVID_PINK");
 
-        if(Target) {
-            let i = 0;
-            const filtered = [];
-            (await Messages).filter((m) => {
-                if(m.author.id === Target.id && Amount > i) {
-                    filtered.push(m);
-                    i++;
-                }
-            })
-
-            await channel.bulkDelete(filtered, true).then(messages => {
-                Response.setDescription(`🧹 Cleared ${messages.size} from ${Target}.`);
-                interaction.reply({embeds: [Response]});
-                logger.info(`${messages.size} mensagens do usuário ${Target} foram apagadas usando o comando clear`)
-            })
-        } else {
-            await channel.bulkDelete(Amount, true).then(messages => {
-                Response.setDescription(`🧹 Cleared ${messages.size} from this channel.`);
-                interaction.reply({embeds: [Response]});
-                logger.info(`${messages.size} mensagens foram apagadas usando o comando clear`)
-            })
+        try {
+            if(Target) {
+                let i = 0;
+                const filtered = [];
+                (await Messages).filter((m) => {
+                    if(m.author.id === Target.id && Amount > i) {
+                        filtered.push(m);
+                        i++;
+                    }
+                })
+    
+                await channel.bulkDelete(filtered, true).then(messages => {
+                    Response.setDescription(`🧹 Cleared ${messages.size} from ${Target}.`);
+                    interaction.reply({embeds: [Response]});
+                    logger.info(`${messages.size} mensagens do usuário ${Target} foram apagadas usando o comando clear`)
+                })
+            } else {
+                await channel.bulkDelete(Amount, true).then(messages => {
+                    Response.setDescription(`🧹 Cleared ${messages.size} from this channel.`);
+                    interaction.reply({embeds: [Response]});
+                    logger.info(`${messages.size} mensagens foram apagadas usando o comando clear`)
+                })
+            }
+        } catch (e) {
+            console.log(e);
         }
+        
     }
 }
