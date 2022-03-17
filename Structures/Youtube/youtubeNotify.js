a9c8ace8c74b74354edbed67a8864509fbbc74ce
Gabriel Franzeri <gabriel.franzeri@elgin.com.br>
2022-03-17 07:41:04 -0300

    test solve bug of youtube updates

--- Structures/Youtube/youtubeNotify.js
+++ Structures/Youtube/youtubeNotify.js
@@ -3,6 +3,10 @@ const { WebhookClient, MessageEmbed } = require('discord.js')
 const spawn = require('child_process').spawn
 const videosDB = require('../Schemas/YoutubeUpdates')
 const mongoose = require('mongoose')
+const { performChannelPageRequestWithFallbacks } = require('yt-channel-info/app/helper')
+const { childrenIgnored } = require('glob/common')
+const { resolve } = require('path')
+const { rejects } = require('assert')
 const databaseURL = process.env.DATABASEURL
 
 connectDB()
@@ -11,34 +15,25 @@ let runScript = new cron.CronJob('*/20 * * * * *', youtubeSearch)
 runScript.start()
 
 async function youtubeSearch() { 
-
-    try {
-        
-        let scriptOutput = ""
-    
-        const pythonProcess = spawn('python', ['youtube-search.py'])
-        pythonProcess.stdout.setEncoding('utf8')
-        pythonProcess.stdout.on('data', (data) => {
-            console.log(data)
-            scriptOutput = JSON.parse(data)
+    console.time('codezup')
+    spawnChild().then(
+       data => {
+           console.log("async result:\n" + data);
+           scriptOutput = JSON.parse(data)
             if(scriptOutput.error === "false"){
                 videoAlreadySent(scriptOutput.id,
-                                 scriptOutput.linkVideo,
-                                 scriptOutput.thumbnail,
-                                 scriptOutput.title,
-                                 scriptOutput.thumbnail,
-                                 scriptOutput.channel)
+                                scriptOutput.linkVideo,
+                                scriptOutput.thumbnail,
+                                scriptOutput.title,
+                                scriptOutput.thumbnail,
+                                scriptOutput.channel)
             } else {
                 console.log(scriptOutput.error)
-            }
-        })
-        pythonProcess.on('close', (code) => {
-            console.log('closing code: ' + code)
-        })
-        
-    } catch (error) {
-        console.log('erro no processo python: ', error)     
-    }
+            }    
+        },
+       err =>  {console.error("async error:\n" + err);}
+   );   
+   console.timeEnd('codezup')
 }
 
 function connectDB(){
@@ -50,6 +45,37 @@ function connectDB(){
     }).catch((err) => {
         console.log(err)
     })
+}
+
+async function spawnChild(){
+    try {
+        
+        let scriptOutput = ""
+    
+        const pythonProcess = spawn('python', ['youtube-search.py'])
+        pythonProcess.stdout.setEncoding('utf8')
+        
+        let data = ""
+        for await (const chunk of pythonProcess.stdout) {
+            console.log('stdout chunk: ' + chunk)
+            data += chunk
+        }
+        let error = ""
+        for await (const chunk of pythonProcess.stderr) {
+            console.error('stdeer chunk: ' + chunk)
+            error += chunk
+        }
+        const exitCode = await new Promise( (resolve, reject) => {
+            pythonProcess.on('close', resolve)
+        })
+        if (exitCode) {
+            throw new Error(`subprocess error exit ${exitCode}, ${error}`)
+        }
+        return data
+        
+    } catch (error) {
+        console.log('erro no processo python: ', error)     
+    }
 }
 
 async function videoAlreadySent(_id, _link, _icon_url, _title, _thumb, _channel){
