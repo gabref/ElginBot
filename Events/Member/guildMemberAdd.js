3338fbc4d466462938ca9e38472ccd6dfb892945
Gabriel Franzeri <gabriel.franzeri@elgin.com.br>
2022-04-02 18:35:13 -0300

    canvas guildMemberAdd

--- Events/Member/guildMemberAdd.js
+++ Events/Member/guildMemberAdd.js
@@ -1,4 +1,5 @@
-const { MessageEmbed, WebhookClient, GuildMember, Message} = require('discord.js')
+const { MessageEmbed, WebhookClient, GuildMember, MessageAttachment } = require('discord.js')
+const Canvas = require('canvas')
 
 module.exports = {
     name: "guildMemberAdd",
@@ -6,7 +7,7 @@ module.exports = {
      * 
      * @param {GuildMember} member 
      */
-    execute(member) {
+    async execute(member) {
         
         const { user, guild } = member
 
@@ -16,17 +17,61 @@ module.exports = {
             token: "-fAIWh5XSJuHEjcNBFa3akPf22bQGPjx9hVR_6rgYjyIbxHMmshAX4esxBCMkVtNjedK"
         })
 
-        const Welcome = new MessageEmbed()
-        .setColor("AQUA")
-        .setAuthor({
-            name: user.tag, 
-            iconURL: user.avatarURL({dynamic: true, size: 512})})
-        .setThumbnail(user.avatarURL({dynamic: true, size: 512}))
-        .setDescription(`
-        Bem-vindo ${member} para o servidor do **${guild.name}**!\n
-        Conta Criada: <t:${parseInt(user.createdTimestamp / 1000)}:R>\nÚltima contagem de membros: **${guild.memberCount}**`)
-        .setFooter({text: `ID: ${user.id}`})
+        const welcomeCanvas = Canvas.createCanvas(1280/2, 595/2)
+        const ctx = welcomeCanvas.getContext('2d')
 
-        Welcomer.send({embeds: [Welcome]})
+        const cw = welcomeCanvas.width
+        const ch = welcomeCanvas.height
+        const cx = cw / 2
+        const cy = ch / 4.2
+        const cr = (ch / 2.6) / 2
+
+        ctx.font = '72px sans-serif'
+        ctx.fillStyle = '#ffffff'
+
+        const background = await Canvas.loadImage('./Structures/Images/background.jpeg')
+        // This uses the canvas dimensions to stretch the image onto the entire canvas
+        ctx.drawImage(background, 0, 0, cw, ch);
+
+        // titulo card
+        // elgin
+        const wr = cw / 2 - cr - 40
+        const wh = wr / 2.3394
+        const logoImageElgin = await Canvas.loadImage('./Structures/Images/logotipo_branco_positivo_1.png')
+        const logoImageDC = await Canvas.loadImage('./Structures/Images/logotipo_branco_positivo_2.png')
+        ctx.drawImage(logoImageElgin, 20, cy - (wh / 2), wr, wh)
+        ctx.drawImage(logoImageDC, cw / 2 + cr + 30, cy - (wh / 2) + 10, wr - 20, wh)
+
+        // bem vindo
+        ctx.textAlign = 'center'
+        ctx.font = '50px sans-serif'
+        ctx.fillText('Bem Vindo', cw / 2, ch / 1.6)
+
+        // draw circle for user image
+        ctx.beginPath()
+        ctx.arc(cx, cy, cr, 0, 2 * Math.PI)
+        ctx.stroke()
+        ctx.fill()
+
+        ctx.font = '28px sans-serif'
+        ctx.fillText(`${user.tag}`, cw / 2, ch / 1.3)
+
+        ctx.font = '22px sans-serif'
+        ctx.fillText(`Agora somos ${guild.memberCount} usuários`, cw / 2, ch / 1.125)
+
+        ctx.beginPath()
+        ctx.arc(cx, cy, cr - 5, 0, 2 * Math.PI)
+        ctx.closePath()
+        ctx.clip()
+
+        const userImage = await Canvas.loadImage(user.displayAvatarURL({ format: 'png', size: 1024 }))
+        // This uses the canvas dimensions to stretch the image onto the entire canvas
+        ctx.drawImage(userImage, cx - cr, cy - cr, 2 * cr, 2 * cr);
+    
+        // Use the helpful Attachment class structure to process the file for you
+        const attachment = new MessageAttachment(welcomeCanvas.toBuffer(), `welcome-${member.id}.png`);
+    
+        Welcomer.send({ files: [attachment] });
+        
     }
 }
